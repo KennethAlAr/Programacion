62696fc8cbcedfc1f64ddb5d202d040bde87b1b5
KennethAlAr <kennethalonsoarce@gmail.com>
2025-03-31 15:05:21 +0200

    Hechos ejercicios del 5 al 10

--- Clases/src/Clase6_20250326/Clase6.java
+++ Clases/src/Clase6_20250326/Clase6.java
@@ -1,6 +1,8 @@
 package Clase6_20250326;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Clase6 {
     public static void main(String[] args) {
@@ -58,40 +60,124 @@ public class Clase6 {
             }
             System.out.println(letra);
         }
+        System.out.println("\n");
 
     //Ejercicio 5 – Dividir dos números con try-catch
     //Haz un programa que divida dos números enteros introducidos por el usuario.
     //Usa try-catch para evitar que el programa se rompa si el divisor es 0.
 
+        System.out.println("Introduce el numerador de la división:");
+        int numerador = sc.nextInt();
+        System.out.println("Introduce el denominador de la división:");
+        int denominador = sc.nextInt();
 
+        try {
+            int resultado = numerador / denominador;
+            System.out.println("El resultado de tu división es: " + resultado);
+        } catch (ArithmeticException e){
+            System.out.println("¡Has intentado dividir entre cero!");
+        }
+        //No sé por qué, si la división es con doubles, dividir entre cero no da error, el resultado es "Infinity".
+        System.out.println("\n");
 
     //Ejercicio 6 – Acceder a una posición no válida de una cadena (charAt) con control de excepción
     //Solicita una palabra al usuario y pide una posición. Intenta mostrar el carácter en esa posición usando try-catch.
     //Si se produce un error, muestra un mensaje.
 
+        System.out.println("Introduce una cadena:");
+        sc.nextLine();
+        String cadena = sc.nextLine();
 
+        System.out.println("Qué posición de la cadena quieres ver: ");
+        int posicion = sc.nextInt();
+
+        try {
+            char letra_cadena = cadena.charAt(posicion);
+            System.out.println("El carácter en la posición " + posicion + " de la cadena "
+                    + cadena + " es '" +letra_cadena + "'");
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("La cadena que has indicado no tiene una posición " + posicion);
+        }
+        System.out.println("\n");
 
     //Ejercicio 7 – Mensaje de fin con finally
     //Haz un programa que intente dividir dos números.
     //Usa try-catch-finally para mostrar siempre un mensaje final que diga "Operación terminada".
 
+        System.out.println("Introduce un número entero a dividir:");
+        int numerador_ej7 = sc.nextInt();
 
+        System.out.println("Introduce un número entero para dividir el anterior:");
+        int denominador_ej7 = sc.nextInt();
+
+        try {
+            int resultado_ej7 = numerador_ej7 / denominador_ej7;
+            System.out.println("El resultado de tu división es: " + resultado_ej7);
+        } catch (ArithmeticException e) {
+            System.out.println("Has intentado dividir entre cero.");
+        } finally {
+            System.out.println("Operación terminada");
+        }
+        System.out.println("\n");
 
     //Ejercicio 8 – Leer números y sumar los positivos, ignorando los negativos con continue
     //Pide al usuario 5 números enteros. Suma solo los positivos usando continue para ignorar los negativos.
 
+        int resultado_ej8 = 0;
 
+        for (int i = 0; i <= 4; i++) {
+            if (i==0){
+                System.out.println("Introduce un número entero, puede ser negativo o positivo: ");
+            } else if (i > 0 && i < 4) {
+                System.out.println("Introduce otro número entero, puede ser negativo o positivo: ");
+            } else {
+                System.out.println("Introduce un último número entero, puede ser negativo o positivo: ");
+            }
+            int numero_ej8 = sc.nextInt();
+            if (numero_ej8 < 0) {
+                continue;
+            }
+            resultado_ej8 = resultado_ej8 + numero_ej8;
+        }
+
+        System.out.println("El resultado de sumar los números positivos que has introducido es: " + resultado_ej8);
+        System.out.println("\n");
 
     //Ejercicio 9 – Detectar si se ha introducido texto en lugar de número (try-catch)
     //Pide al usuario que introduzca un número. Si escribe una palabra u otro texto,
     //el programa debe capturar la excepción y mostrar "Eso no es un número".
 
+        System.out.println("Introduce un número: ");
 
+        try {
+            double numero_ej9 = sc.nextDouble();
+            System.out.println("Tu número es: " + numero_ej9);
+        } catch (InputMismatchException e) {
+            System.out.println("Eso no es un número.");
+        }
+        System.out.println("\n");
 
     //Ejercicio 10 – Salir de un bucle do-while cuando se adivina un número secreto
     //Crea un número secreto entre 1 y 10. El usuario tiene que adivinarlo.
     //Usa do-while y break para salir cuando acierte.
 
+        Random random = new Random();
+        int numero_random = random.nextInt(1,11);
+        int respuesta;
+
+        System.out.println("Intenta adivinar el número secreto del 1 al 10:");
+
+        do {
+            respuesta = sc.nextInt();
+            if (respuesta < numero_random) {
+                System.out.println("Incorrecto, el número es más alto.");
+            } else if (respuesta > numero_random) {
+                System.out.println("Incorrecto, el número es más bajo.");
+            } else if (respuesta == numero_random) {
+                System.out.println("Correcto, el número secreto era " + numero_random);
+                break;
+            }
+        } while(true);
 
         sc.close();
     }
